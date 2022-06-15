65b07339775a4db166ae21a5d23975511be2f0d7
seregakov <103594115+seregakov@users.noreply.github.com>
2022-06-15 18:00:54 +0300

    Update hw1.php

--- hw1.php
+++ hw1.php
@@ -1,38 +1,38 @@
 <?php 
 
 
-// $a = 1;
-// $b = 4;
-// $c = 2;
+$a = 1;
+$b = 4;
+$c = 2;
 
 
-// if ($a >= $b) {
-//   if ($a > $c){
-//     echo $a;
-//   } else {
-//     echo $c;
-//   }
-// } elseif ($b >= $c) {
-//   echo $b;
-// } else {
-//   echo $c;
-// }
+if ($a >= $b) {
+  if ($a > $c){
+    echo $a;
+  } else {
+    echo $c;
+  }
+} elseif ($b >= $c) {
+  echo $b;
+} else {
+  echo $c;
+}
 
 
-// $a = 3;
-// $b = 5;
-// $c = 4;
+$a = 3;
+$b = 5;
+$c = 4;
 
 
-// if ($a == $b  $a == $c  $b == $c) {
-//   echo 'ошибка';
-// } elseif ($b > $a && $b < $c  $b < $a && $b > $c) {
-//   echo $b;
-// } elseif ($a > $b && $a < $c  $a < $b && $a > $c) {
-//   echo $a;
-// } else {
-//   echo $c;
-// }
+if ($a == $b  $a == $c  $b == $c) {
+  echo 'ошибка';
+} elseif ($b > $a && $b < $c || $b < $a && $b > $c) {
+  echo $b;
+} elseif ($a > $b && $a < $c || $a < $b && $a > $c) {
+  echo $a;
+} else {
+  echo $c;
+}
 
 
 
